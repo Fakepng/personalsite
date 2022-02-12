09f85415cfc59e24d04e1bc7531c6e2161d47eea
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-02-12 15:33:28 +0700

    Create Project page

--- src/Pages/Project/index.jsx
+++ src/Pages/Project/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Project.css'
 
 const Project = () => {
+  const [repos, setRepos] = useState([]);
+
+  async function getRepos() {
+    const response = await fetch('https://api.github.com/users/Fakepng/repos');
+    const responseJson = await response.json();
+    setRepos(responseJson);
+  }
+
+  useEffect(() => {
+    getRepos();
+  }, []);
+
+  const repoElement = repos.map(repo => {
+    return (
+      <div className="project-container">
+        <a href={repo.html_url}>
+          <div className="project-title">
+            {repo.name}
+          </div>
+        </a>
+      </div>
+    )
+  })
+
   return (
-    <div className="Project">Project</div>
+    <>
+      <h1>Projects on GitHub</h1>
+      <div className="project">
+        {repoElement}
+      </div>
+    </>
   )
 }
 
