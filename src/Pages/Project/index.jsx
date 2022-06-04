89c5d809a762c3beb5f4cbf7a14666eb3d4cc118
Fakepng <krit0kasemtewin@gmail.com>
2022-06-05 00:23:24 +0700

    Update index.jsx

--- src/Pages/Project/index.jsx
+++ src/Pages/Project/index.jsx
@@ -1,39 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import './Project.css'
+import React, { useState, useEffect } from "react";
+import "./Project.css";
 
 const Project = () => {
-  const [repos, setRepos] = useState([]);
+	const [repos, setRepos] = useState([]);
 
-  async function getRepos() {
-    const response = await fetch('https://api.github.com/users/Fakepng/repos');
-    const responseJson = await response.json();
-    setRepos(responseJson);
-  }
+	async function getRepos() {
+		const response = await fetch("https://api.github.com/users/Fakepng/repos");
+		const responseJson = await response.json();
+		setRepos(responseJson);
+	}
 
-  useEffect(() => {
-    getRepos();
-  }, []);
+	useEffect(() => {
+		getRepos();
+	}, []);
 
-  const repoElement = repos.map(repo => {
-    return (
-      <div className="project-container" key={repo.id}>
-        <a href={repo.html_url}>
-          <div className="project-title">
-            {repo.name}
-          </div>
-        </a>
-      </div>
-    )
-  })
+	const repoElement = repos.map(async (repo) => {
+		return (
+			<div className='project-container' key={repo.id}>
+				<a href={repo.html_url}>
+					<div className='project-title'>{repo.name}</div>
+				</a>
+			</div>
+		);
+	});
 
-  return (
-    <>
-      <h1>Projects on GitHub</h1>
-      <div className="project">
-        {repoElement}
-      </div>
-    </>
-  )
-}
+	return (
+		<>
+			<h1>Projects on GitHub</h1>
+			<div className='project'>{repoElement}</div>
+		</>
+	);
+};
 
-export default Project
+export default Project;
