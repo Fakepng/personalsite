ff74bf57a482d8449e850f2809ffc94c5b4b2e1f
Fakepng <krit0kasemtewin@gmail.com>
2022-06-10 23:45:10 +0700

    Create: navigation menu

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Hamburger from "hamburger-react";
+import "./Menu.css";
+
+const Menu = () => {
+	const [isOpen, setOpen] = useState(false);
+
+	const closeHandler = () => {
+		setOpen(false);
+	};
+
+	return (
+		<div className='menu-container'>
+			<div className='hamburger'>
+				<Hamburger color='#E7DFDD' rounded toggled={isOpen} toggle={setOpen} />
+			</div>
+			<div className={isOpen ? "menu-open" : "menu-close"}>
+				<a href='#hero' onClick={closeHandler}>
+					Top
+				</a>
+				<a href='#aboutme' onClick={closeHandler}>
+					About me
+				</a>
+				<a href='#github' onClick={closeHandler}>
+					Github
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Menu;
