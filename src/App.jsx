7f574db667e05bf68d720947e6ce871d14946283
Fakepng <krit0kasemtewin@gmail.com>
2022-06-10 23:49:52 +0700

    Add: react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Hero from "./pages/Hero";
 import AboutMe from "./pages/AboutMe";
+import Github from "./pages/Github";
+import Menu from "./components/Menu";
 
 function App() {
 	return (
-		<>
-			<Hero />
-			<AboutMe />
-		</>
+		<BrowserRouter>
+			<Menu />
+			<Routes>
+				<Route
+					exact
+					path='/'
+					element={
+						<>
+							<Hero />
+							<AboutMe />
+							<Github />
+						</>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
