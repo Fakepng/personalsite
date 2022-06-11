c78dba87e73817636ad1b00dfeaff9e8e8f6a0a5
Fakepng <krit0kasemtewin@gmail.com>
2022-06-11 18:27:18 +0700

    Finish: Github section

--- src/pages/Github/index.jsx
+++ src/pages/Github/index.jsx
@@ -1,9 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./Github.css";
 
 const Github = () => {
+	const [repos, setRepos] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("https://api.github.com/users/Fakepng/repos")
+			.then((res) => {
+				setRepos(res.data);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	const reposList = repos.map((repo) => {
+		return (
+			<div className={`repo-container ${repo.language}`} key={repo.id}>
+				<abbr title={repo.description}>
+					<a href={repo.html_url} target='_blank' rel='external'>
+						<h2>{repo.name}</h2>
+						<p>{repo.language}</p>
+					</a>
+				</abbr>
+			</div>
+		);
+	});
+
 	return (
 		<div id='github' className='Github'>
-			<h1>Github</h1>
+			{reposList}
 		</div>
 	);
 };
