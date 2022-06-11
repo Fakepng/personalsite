3ee4c8089731699bd27c6c52faaccb61db37c229
Fakepng <krit0kasemtewin@gmail.com>
2022-06-11 17:18:35 +0700

    Update index.jsx

--- src/Pages/Project/index.jsx
+++ src/Pages/Project/index.jsx
@@ -3,25 +3,11 @@ import "./Project.css";
 
 const Project = () => {
 	const [repos, setRepos] = useState([]);
-	const [isRateLimited, setIsRateLimited] = useState(false);
-
-	const APIREATELIMITEXCEEDED = [
-		{
-			name: "API Rate Limit Exceeded",
-			html_url: "",
-			id: "API Rate Limit Exceeded",
-		},
-	];
 
 	async function getRepos() {
 		const response = await fetch("https://api.github.com/users/Fakepng/repos");
 		const responseJson = await response.json();
-		if (responseJson.message?.includes("API rate limit exceeded")) {
-			console.log("API rate limit exceeded");
-			setIsRateLimited(true);
-		} else {
-			setRepos(responseJson);
-		}
+		setRepos(responseJson);
 	}
 
 	useEffect(() => {
@@ -41,11 +27,7 @@ const Project = () => {
 	return (
 		<>
 			<h1>Projects on GitHub</h1>
-			{isRateLimited ? (
-				<h2 className='apiratelimitexceeded'>API rate limit exceeded</h2>
-			) : (
-				<div className='project'>{repoElement}</div>
-			)}
+			<div className='project'>{repoElement}</div>
 		</>
 	);
 };
