34baafef2f8d738a65e989bffb85851f1ab9b964
Fakepng <krit0kasemtewin@gmail.com>
2022-06-25 23:05:40 +0700

    Remove: about me

--- src/pages/AboutMe/index.jsx
+++ src/pages/AboutMe/index.jsx
@@ -4,26 +4,12 @@ const AboutMe = () => {
 	const MY_BIRTHDAY = 1089872520000;
 	const MS_TO_YEAR = 31536000000;
 	const now = new Date().getTime();
-	console.log(now);
 	const age = Math.floor((now - MY_BIRTHDAY) / MS_TO_YEAR);
 
 	return (
 		<div id='aboutme' className='AboutMe'>
 			<h1>AboutMe</h1>
-			<div>
-				<p>{"{"}</p>
-				<p>&emsp;"name": "Krit Kasemtewin",</p>
-				<p>&emsp;"nickname": "Krit",</p>
-				<p>
-					&emsp;"age": <span>{age}</span>,
-				</p>
-				<p>&emsp;"school": "Satriwitthaya 2 school"</p>
-				<p>
-					&emsp;"interests": {"["}Space, EV, AI, Smart Home, IoT
-					{"]"}
-				</p>
-				<p>{"}"}</p>
-			</div>
+			<div></div>
 		</div>
 	);
 };
