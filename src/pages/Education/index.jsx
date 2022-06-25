b70ab8c734718159f0fc282c580f6f9a8c9a8899
Fakepng <krit0kasemtewin@gmail.com>
2022-06-25 23:05:15 +0700

    Create: education section

---
+++ src/pages/Education/index.jsx
@@ -0,0 +1,26 @@
+import Educations from "../../config/Education.json";
+import "./Education.css";
+
+const education = Educations.reverse().map((education) => {
+	return (
+		<div className='edu' key={education.id}>
+			<img src={`/images/school/${education.image}`} alt={education.name} />
+			<h3>{education.year}</h3>
+			<h4>{education.name}</h4>
+			<h4>{education.level}</h4>
+			<h5>{education.sec}</h5>
+			<h5>{education.gpa}</h5>
+		</div>
+	);
+});
+
+const Education = () => {
+	return (
+		<div id='education' className='Education'>
+			<h1>Education</h1>
+			<div className='edu-container'>{education}</div>
+		</div>
+	);
+};
+
+export default Education;
