c4cb9cf09a05fc86a2b79e518f6f378277cecd93
Fakepng <krit0kasemtewin@gmail.com>
2022-07-10 22:35:12 +0700

    Comment unused code

--- src/pages/AboutMe/index.jsx
+++ src/pages/AboutMe/index.jsx
@@ -1,10 +1,10 @@
 import "./AboutMe.css";
 
 const AboutMe = () => {
-	const MY_BIRTHDAY = 1089872520000;
-	const MS_TO_YEAR = 31536000000;
-	const now = new Date().getTime();
-	const age = Math.floor((now - MY_BIRTHDAY) / MS_TO_YEAR);
+	// const MY_BIRTHDAY = 1089872520000;
+	// const MS_TO_YEAR = 31536000000;
+	// const now = new Date().getTime();
+	// const age = Math.floor((now - MY_BIRTHDAY) / MS_TO_YEAR);
 
 	return (
 		<div id='aboutme' className='AboutMe'>
