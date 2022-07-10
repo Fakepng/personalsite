e0b82f254f821fa4e75bbca8d976b05121b128e9
Fakepng <krit0kasemtewin@gmail.com>
2022-07-10 22:35:53 +0700

    Remove unused Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Hero from "./pages/Hero";
 import AboutMe from "./pages/AboutMe";
